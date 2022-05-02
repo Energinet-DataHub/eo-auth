5c066bf99749297de3522fa2f4e6e00bba42900f
Claus Christiansen <claus.christiansen1987@gmail.com>
2022-05-02 12:34:12 +0000

    Redone conftest to reduce multiple of same fixture.

--- tests/auth_api/conftest.py
+++ tests/auth_api/conftest.py
@@ -10,7 +10,7 @@ from typing import Dict, Any
 from unittest.mock import patch
 from authlib.jose import jwt, jwk
 from flask.testing import FlaskClient
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from testcontainers.postgres import PostgresContainer
 
 from origin.tokens import TokenEncoder
@@ -30,6 +30,7 @@ from auth_api.config import (
 )
 
 from .keys import PRIVATE_KEY, PUBLIC_KEY
+
 
 # -- API ---------------------------------------------------------------------
 
@@ -95,6 +96,19 @@ def jwk_private() -> str:
     """Mock private key from Identity Provider."""
     return jwk.dumps(PRIVATE_KEY, kty='RSA')
 
+
+@pytest.fixture(scope='function')
+def subject() -> str:
+    """Return the subject."""
+
+    return 'subject'
+
+
+@pytest.fixture(scope='function')
+def actor() -> str:
+    """Return an actor name."""
+
+    return 'actor'
 
 # -- Tokens ------------------------------------------------------------------
 
@@ -192,7 +206,7 @@ def ip_token(
 
 
 @pytest.fixture(scope='function')
-def id_token(
+def oidc_id_token(
         token_subject: str,
         token_idp: str,
         token_issued: datetime,
@@ -229,13 +243,13 @@ def id_token(
 @pytest.fixture(scope='function')
 def id_token_encoded(
         jwk_private: str,
-        id_token: Dict[str, Any],
+        oidc_id_token: Dict[str, Any],
 ) -> str:
     """Mock ID-token from Identity Provider (encoded)."""
 
     token = jwt.encode(
         header={'alg': 'RS256'},
-        payload=id_token,
+        payload=oidc_id_token,
         key=jwk_private,
     )
 
@@ -286,6 +300,82 @@ def userinfo_token(
         "transaction_id": token_transaction_id,
         "aud": token_aud,
     }
+
+
+@pytest.fixture(scope='function')
+def opaque_token() -> str:
+    """
+    Return Opaque token.
+
+    Return a opaque token, which are the token
+    that are actual visible to the frontend.
+    """
+
+    return str(uuid4())
+
+
+@pytest.fixture(scope='function')
+def issued_datetime() -> datetime:
+    """Datetime that indicates when a token has been issued."""
+
+    return datetime.now(tz=timezone.utc)
+
+
+@pytest.fixture(scope='function')
+def expires_datetime() -> datetime:
+    """Datetime that indicates when a token will expire."""
+
+    return datetime.now(tz=timezone.utc) + timedelta(days=1)
+
+
+@pytest.fixture(scope='function')
+def internal_token(
+    subject: str,
+    expires_datetime: datetime,
+    issued_datetime: datetime,
+    actor: str,
+) -> InternalToken:
+    """Return the internal token used within the system itself."""
+
+    return InternalToken(
+        issued=issued_datetime,
+        expires=expires_datetime,
+        actor=actor,
+        subject=subject,
+        scope=['scope1', 'scope2'],
+    )
+
+
+@pytest.fixture(scope='function')
+def internal_token_encoded(
+    internal_token: InternalToken,
+    internal_token_encoder: TokenEncoder[InternalToken],
+) -> str:
+    """Return the internal token in encoded string format."""
+
+    return internal_token_encoder \
+        .encode(internal_token)
+
+
+@pytest.fixture(scope='function')
+def id_token() -> str:
+    """Return a dummy identity provider id_token."""
+
+    return 'id-token'
+
+
+@pytest.fixture(scope='function')
+def ssn() -> str:
+    """Return a dummy identity provider id_token."""
+
+    return '123456789'
+
+
+@pytest.fixture(scope='function')
+def tin() -> str:
+    """Return a dummy identity provider id_token."""
+
+    return '987654321'
 
 
 # # -- SQL --------------------------------------------------------------------
