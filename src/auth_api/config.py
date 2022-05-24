ef2886e5050d49408998c765d72647a7cffb367a
rasmus godske <rasmusgodske@gmail.com>
2022-05-24 13:59:50 +0000

    added comment

--- src/auth_api/config.py
+++ src/auth_api/config.py
@@ -111,5 +111,9 @@ OIDC_API_LOGOUT_URL = f'{OIDC_AUTHORITY_URL}/api/v1/session/logout'
 
 # -- eo-datasync -------------------------------------------------------------
 DATASYNC_BASE_URL = os.environ.get('DATASYNC_BASE_URL', 'http://eo-data-sync/')
+
+# TODO: This subpath should be temporary and be changed to /relations instead
+# This is so that it matches up with the documentation at:
+# github.com/Energinet-DataHub/energy-origin/blob/main/doc/api/datasync.md
 DATASYNC_CREATE_RELATIONS_PATH = \
     os.environ.get('DATASYNC_CREATE_RELATIONS_PATH', '/Relation/relations')
