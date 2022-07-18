462dc484be2c4a915fe6dc66a6dbf41d4e1f6cdd
Exobitt <dannip1123@outlook.com>
2022-07-18 10:27:59 +0000

    lint

--- src/auth_api/templates/logging_templates.py
+++ src/auth_api/templates/logging_templates.py
@@ -18,7 +18,7 @@ class LoggingTemplates:
         self.log_level = log_level
 
     def log(self, message, actor, subject) -> None:
-        """TODO."""
+        """This logs a certain template."""
 
         time_stamp = datetime.now().strftime('%Y-%m-%dT%Y:%H:%M:%S.%f')
 
